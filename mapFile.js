24e342d7589b9782048f4df95590ed37817fd0b5
Daniele Irsuti <irsooti@gmail.com>
2017-04-18 21:06:55 +0200

    merge with WIP

---
+++ mapFile.js
@@ -0,0 +1,26 @@
+// Written in Tue Apr 18 2017 20:53:02 GMT+0200 (W. Europe Daylight Time)
+{
+    "js": [
+        "abruzzoHigh.js",
+        "basilicataHigh.js",
+        "calabriaHigh.js",
+        "campaniaHigh.js",
+        "emiliaromagnaHigh.js",
+        "friuliHigh.js",
+        "italyHigh.js",
+        "lazioHigh.js",
+        "liguriaHigh.js",
+        "lombardiaHigh.js",
+        "marcheHigh.js",
+        "moliseHigh.js",
+        "piemonteHigh.js",
+        "pugliaHigh.js",
+        "sardegnaHigh.js",
+        "siciliaHigh.js",
+        "toscanaHigh.js",
+        "trentinoHigh.js",
+        "umbriaHigh.js",
+        "valledaostaHigh.js",
+        "venetoHigh.js"
+    ]
+}
